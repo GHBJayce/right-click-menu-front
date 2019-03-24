b6fd23d74a8a0723562617ba3bca430c92c46f5d
GHBJayce <jaycedeng@outlook.com>
2019-03-24 23:05:22 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,11 @@
                 <b-card title="操作台" bg-variant="white" border-variant="light" class="shadow-sm">
                     <el-tabs v-model="activeName">
                         <el-tab-pane label="右键菜单" name="rcm">
+                            <div class="text-left">
+                                <b-button @click="createMenu" variant="primary" size="sm">新增菜单</b-button>
+                            </div>
                             <el-tree
+                                ref="rcm"
                                 draggable
                                 show-checkbox
                                 node-key="id"
@@ -16,6 +20,92 @@
                                 @node-click="handleNodeClick">
                             </el-tree>
                             <b-button variant="primary" block class="w-100">生成</b-button>
+                            <el-dialog title="新增菜单" :visible.sync="createMenuDialogVisible">
+                                <el-form label-position="right" label-width="80px" :model="currentMenuDrawer.data">
+                                    <el-form-item label="注册表名">
+                                        <el-input v-model="currentMenuDrawer.data.id"></el-input>
+                                    </el-form-item>
+                                    <el-form-item label="菜单名称">
+                                        <el-input v-model="currentMenuDrawer.data.label"></el-input>
+                                    </el-form-item>
+                                    <el-form-item label="程序路径">
+                                        <el-input v-model="currentMenuDrawer.data.path"></el-input>
+                                    </el-form-item>
+                                    <el-form-item label="菜单图标">
+                                        <el-input v-model="currentMenuDrawer.data.icon"></el-input>
+                                    </el-form-item>
+                                    <b-button variant="primary" block class="w-100 mt-5">创建</b-button>
+                                </el-form>
+                                <p>选择</p>
+                                <el-tree
+                                    ref="menuSets"
+                                    draggable
+                                    show-checkbox
+                                    node-key="id"
+                                    class="mt-2 mb-4"
+                                    :data="tree.menuSets.data"
+                                    :default-expanded-keys="tree.menuSets.expandKeys"
+                                    :allow-drag="menuSetsAllowDrag"
+                                    @node-click="menuSetsClick">
+                                </el-tree>
+                            </el-dialog>
+                            <a-drawer
+                                title="菜单设置"
+                                placement="right"
+                                :visible="rcmDrawer.show"
+                                :width="rcmDrawer.width"
+                                @close="onClose"
+                            >
+                                <el-form ref="form" :model="form" label-width="80px">
+                                    <el-form-item label="注册表名">
+                                        <el-input v-model="form.name"></el-input>
+                                    </el-form-item>
+                                    <el-form-item label="菜单名称">
+                                        <el-input v-model="form.name"></el-input>
+                                    </el-form-item>
+                                    <el-form-item label="菜单图标">
+                                        <el-input v-model="form.name"></el-input>
+                                    </el-form-item>
+                                    <el-form-item label="菜单位置">
+                                        <el-select v-model="value5" multiple placeholder="请选择右键菜单的位置" class="w-100">
+                                            <el-option
+                                                v-for="item in options"
+                                                :key="item.value"
+                                                :label="item.label"
+                                                :value="item.value">
+                                            </el-option>
+                                        </el-select>
+                                    </el-form-item>
+                                    <el-form-item label="特殊资源">
+                                        <el-radio-group v-model="form.resource">
+                                        <el-radio label="线上品牌商赞助"></el-radio>
+                                        <el-radio label="线下场地免费"></el-radio>
+                                        </el-radio-group>
+                                    </el-form-item>
+                                    <el-form-item>
+                                        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+                                        <el-button>取消</el-button>
+                                    </el-form-item>
+                                </el-form>
+                                <el-row>
+                                    <el-col :span="24" class="mb-2">
+                                        <strong>注册表名：</strong>
+                                        <span></span>
+                                    </el-col>
+                                    <el-col :span="24" class="mb-2">
+                                        <strong>菜单名称：</strong>
+                                        <span></span>
+                                    </el-col>
+                                    <el-col :span="24" class="mb-2">
+                                        <strong>程序路径：</strong>
+                                        <span></span>
+                                    </el-col>
+                                    <el-col :span="24" class="mb-2">
+                                        <strong>菜单图标：</strong>
+                                        <span></span>
+                                    </el-col>
+                                </el-row>
+                            </a-drawer>
                         </el-tab-pane>
                         <el-tab-pane label="菜单集" name="menuSets">
                             <el-tree
@@ -84,6 +174,11 @@ export default {
                 formLabelPosition: 'top',
                 data: {}
             },
+            rcmDrawer: {
+                width: '720',
+                show: false,
+                data: {}
+            },
             menuVisible: false,
             RCMTreeExpandKeys: ['my-right-click-menu'],
             RCMTree: [{
@@ -97,12 +192,47 @@ export default {
                         setid: 'copy-target-path'
                     }]
                 }]
+            }, {
+                label: '我的右键菜单',
+                id: 'my-right-click-menu',
+                children: [{
+                    label: '复制目标路径',
+                    setid: 'copy-target-path'
+                }]
             }],
             defaultProps: {
                 children: 'children',
                 label: 'label'
             },
-            visible: false
+            visible: false,
+            createMenuDialogVisible: false,
+            options: [{
+                value: '选项1',
+                label: '黄金糕'
+            }, {
+                value: '选项2',
+                label: '双皮奶'
+            }, {
+                value: '选项3',
+                label: '蚵仔煎'
+            }, {
+                value: '选项4',
+                label: '龙须面'
+            }, {
+                value: '选项5',
+                label: '北京烤鸭'
+            }],
+            value5: [],
+            form: {
+                name: '',
+                region: '',
+                date1: '',
+                date2: '',
+                delivery: false,
+                type: [],
+                resource: '',
+                desc: ''
+            }
         }
     },
     mounted () {
@@ -128,7 +258,9 @@ export default {
                     id: 'practical-function',
                     children: [{
                         label: '复制目标路径',
-                        id: 'copy-target-path'
+                        id: 'copy-target-path',
+                        path: 'mshta vbscript:clipboarddata.setdata("text","%1")(close)',
+                        icon: '%SYSTEMROOT%\\explorer.exe'
                     }]
                 }, {
                     label: '电源控制',
@@ -203,8 +335,18 @@ export default {
         },
         onClose: function () {
             this.currentMenuDrawer.show = false
+            this.rcmDrawer.show = false
             let keys = this.$refs.menuSets.getCurrentKey()
             console.log(keys)
+        },
+        handleNodeClick: function (e, node, sel) {
+            console.log(e, node, sel)
+            console.log(this.$refs.menuSets.getNode(e.setid))
+            this.rcmDrawer.show = true
+            node.expanded = true
+        },
+        createMenu: function () {
+            this.createMenuDialogVisible = true
         }
     }
 }
